c2b17f31298694f2d35295d2ed98af0e41aeb755
Diogo Dias <diogo.amorim.dias@hotmail.com>
2021-01-25 10:25:47 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/diogoamorim/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.diogoamorim.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diogoamorim.workshopmongo.domain.User;
+import com.diogoamorim.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+}
