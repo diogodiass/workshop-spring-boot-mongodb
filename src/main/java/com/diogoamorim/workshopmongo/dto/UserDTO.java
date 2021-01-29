4d616ea4c12cb1d05b727655b704136f45d2dc23
Diogo Dias <diogo.amorim.dias@hotmail.com>
2021-01-29 18:44:42 -0300

    Obtendo um usuário por id

--- src/main/java/com/diogoamorim/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/diogoamorim/workshopmongo/dto/UserDTO.java
@@ -5,7 +5,8 @@ import java.io.Serializable;
 import com.diogoamorim.workshopmongo.domain.User;
 
 public class UserDTO implements Serializable {
-
+	private static final long serialVersionUID = 1L;
+	
 	private String id;
 	private String name;
 	private String email;
--- src/main/java/com/diogoamorim/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/diogoamorim/workshopmongo/repository/UserRepository.java
@@ -8,4 +8,6 @@ import com.diogoamorim.workshopmongo.domain.User;
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
+	
+
 }
