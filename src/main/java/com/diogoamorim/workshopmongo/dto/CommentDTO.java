6ebdb360813a252c98d2022e65372d0b44328b2d
Diogo Dias <diogo.amorim.dias@hotmail.com>
2021-02-05 16:11:12 -0300

    consulta simples com query methods

--- src/main/java/com/diogoamorim/workshopmongo/dto/CommentDTO.java
+++ src/main/java/com/diogoamorim/workshopmongo/dto/CommentDTO.java
@@ -19,4 +19,30 @@ public class CommentDTO {
 		this.author = author;
 	}
 
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public AuthorDTO getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
+	}
+	
+	
+
 }
